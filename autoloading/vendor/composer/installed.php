58385305ff93228fd901770ea1fd76f9f47b0e30
dosha10 <ndungufrank01@gmail.com>
2022-10-24 13:53:26 +0300

    composer

---
+++ autoloading/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'frankndungu/php-crash',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f0f01f0b6612a2ecc2c1e0deadc5d0bfe089542b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'frankndungu/php-crash' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f0f01f0b6612a2ecc2c1e0deadc5d0bfe089542b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
